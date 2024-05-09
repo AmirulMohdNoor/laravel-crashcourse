979f18820a230a7535bcbd47f6f5c0cb9e2fe793
AmirulMohdNoor <aamirulamirul@gmail.com>
2024-05-09 04:34:04 +0100

    Add hello route and pass name variable tp hello view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = 'Muhammad Amirul';
+
+    return view('hello', compact('name'));
 });
